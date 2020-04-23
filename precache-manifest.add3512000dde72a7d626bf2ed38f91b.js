1c47208c6c23fee77f4b80c6891afaaae54e7a86
sunjinkimJ <gktjswls33@gmail.com>
2020-04-24 00:14:54 +0900

    Updates

---
+++ precache-manifest.add3512000dde72a7d626bf2ed38f91b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efaaee9e94fca7ff3e7ffa034dc4465f",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "7e2b0fa8ca050962e6b0",
+    "url": "/movie_app/static/css/main.02cc2902.chunk.css"
+  },
+  {
+    "revision": "1d2d5261e5e33a966d0f",
+    "url": "/movie_app/static/js/2.111c2b99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.111c2b99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e2b0fa8ca050962e6b0",
+    "url": "/movie_app/static/js/main.5a1a2aff.chunk.js"
+  },
+  {
+    "revision": "c4e503dc5fc5f4a3fb01",
+    "url": "/movie_app/static/js/runtime-main.202e151f.js"
+  }
+]);
