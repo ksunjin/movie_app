4ee9d29221d7b9a4ad9b8e2bc4769deffe3076a9
sunjinkimJ <gktjswls33@gmail.com>
2020-04-24 01:57:05 +0900

    20200424_react_movie_final

---
+++ src/routes/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About({}){
+    return (
+        <div className = "about__container">
+            <span>wow</span>
+
+        </div>
+    );
+}
+
+export default About;
